432b85a39ccda1e0522d88fc54bd8d698358d4b3
OnurBaha <koconurbaha@gmail.com>
2024-05-09 22:19:57 +0300

    RapidApi: Imdb Top 100 Movies API

---
+++ RapidApi/RapidApiConsume/Controllers/ImdbController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+using System.Net.Http.Headers;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ImdbController : Controller
+    {
+        public async Task< IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovieViewModels = new List<ApiMovieViewModel>();
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "dd5f0b9e17msh70d5fa8d15fc783p1b6f65jsn799f63fbd965" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovieViewModels = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovieViewModels);
+            }
+            
+        }
+    }
+}
