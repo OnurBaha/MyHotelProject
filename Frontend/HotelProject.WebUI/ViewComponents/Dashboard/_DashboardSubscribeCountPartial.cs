c092410224577ef34bf9bc4c427a92094957492d
OnurBaha <koconurbaha@gmail.com>
2024-09-17 19:26:38 +0300

    Fetching Social Media Follower Counts with API.

---
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -0,0 +1,74 @@
+﻿using HotelProject.WebUI.Dtos.FollowersDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.ViewComponents.Dashboard
+{
+    public class _DashboardSubscribeCountPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/bahaonur"),
+                Headers =
+    {
+        { "x-rapidapi-key", "dd5f0b9e17msh70d5fa8d15fc783p1b6f65jsn799f63fbd965" },
+        { "x-rapidapi-host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ResultInstagramFollowersDto resultInstagramFollowersDtos = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
+                ViewBag.v1 = resultInstagramFollowersDtos.followers;
+                ViewBag.v2 = resultInstagramFollowersDtos.following;
+            }
+
+
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://twitter32.p.rapidapi.com/getProfile?username=Sehabe"),
+                Headers =
+    {
+        { "x-rapidapi-key", "dd5f0b9e17msh70d5fa8d15fc783p1b6f65jsn799f63fbd965" },
+        { "x-rapidapi-host", "twitter32.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                ResultTwitterFollowersDto resultTwitterFollowersDto = JsonConvert.DeserializeObject<ResultTwitterFollowersDto>(body2);
+                ViewBag.v3 = resultTwitterFollowersDto.data.user_info.followers_count;
+                ViewBag.v4 = resultTwitterFollowersDto.data.user_info.friends_count;
+            }
+
+            var client3 = new HttpClient();
+            var request3 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile?linkedin_url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fonur-baha-ko%25C3%25A7-360969260%2F&include_skills=false&include_certifications=false&include_publications=false&include_honors=false&include_volunteers=false&include_projects=false&include_patents=false&include_courses=false&include_organizations=false&include_company_public_url=false"),
+                Headers =
+    {
+        { "x-rapidapi-key", "dd5f0b9e17msh70d5fa8d15fc783p1b6f65jsn799f63fbd965" },
+        { "x-rapidapi-host", "fresh-linkedin-profile-data.p.rapidapi.com" },
+    },
+            };
+            using (var response3 = await client3.SendAsync(request3))
+            {
+                response3.EnsureSuccessStatusCode();
+                var body3 = await response3.Content.ReadAsStringAsync();
+                ResultLinkedinFollowersDto resultLinkedinFollowersDto = JsonConvert.DeserializeObject<ResultLinkedinFollowersDto>(body3);
+                ViewBag.v5 = resultLinkedinFollowersDto.data.follower_count;
+                ViewBag.v6 = resultLinkedinFollowersDto.data.connection_count;
+            }
+            return View();
+        }
+    }
+}
